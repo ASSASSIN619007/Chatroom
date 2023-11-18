fbcdf6ca1e169a0c8848a652bba3676f94e1b372
R Kumar Shaanu <kumarshaanu619007@gmail.com>
2023-11-18 19:33:30 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-// /public/javascript.js
+// ./public/javascript.js
 // Get the current username from the cookies
 var user = cookie.get('user');
 
@@ -13,3 +13,31 @@ if (!user) {
     cookie.set('user', user);
   }
 }
+
+var socket = io();
+
+// The user count (can change when someone joins/leaves)
+socket.on('count', function (data) {
+  $('.user-count').html(data);
+});
+
+// When we receive a message (user: 'username', message: 'text')
+socket.on('message', function (data) {
+  $('.chat').append('<p><strong>' + data.user + '</strong>: ' + data.message + '</p>');
+});
+
+// When the form is submitted
+$('form').submit(function (e) {
+  // Avoid submitting via HTTP
+  e.preventDefault();
+
+  // Retrieve the message from the user
+  var message = $(e.target).find('input').val();
+
+  // Send the message to the server
+  socket.emit('message', { user: cookie.get('user') || 'Anonymous', message: message });
+
+  // Clear the input and focus on it for a new message
+  e.target.reset();
+  $(e.target).find('input').focus();
+});
